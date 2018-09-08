1bd1b5d984037c0584b3fc07b4ab533f5691b55b
Andrés Liu <adavidliuf@gmail.com>
2018-09-08 17:41:04 -0500

    added invite-friends page

--- js/modules/menu.js
+++ js/modules/menu.js
@@ -1,12 +1,18 @@
 class Menu {
     constructor() {
         this._$btn = $(".header__btn");
+        this._$nav = $(".nav-mobile");
         this._$body = $("body");
+        this._$wrapper = $(".wrapper");
 
         this._$btn.click(this.toggleMobileNav);
     }
 
     toggleMobileNav = () => {
         this._$body.toggleClass("menu-open");
+        const timeout = setTimeout(() => {
+            this._$nav.css("height", this._$body.innerHeight() + "px");
+            clearTimeout(timeout);
+        }, 250);
     }
 }

76b0ea14fba98bd5c69feb56027b06bf1ce46345
Andrés Liu <adavidliuf@gmail.com>
2018-09-15 20:58:56 -0500

    Added more pages

--- js/app.js
+++ js/app.js
@@ -30,5 +30,172 @@ $(document).ready(function () {
             }
         });
     }
+
+    if ($(".registration__interest").length > 0) {
+        $(".registration__interest").click(e => {
+            const $this = $(e.currentTarget);
+            $this.toggleClass("registration__interest--active");
+        });
+    }
+
+    if ($("[data-max-words]").length > 0) {
+        $("[data-max-words]").each(function(i) {
+            const $this = $(this);
+            const $label = $(`[data-label-target=${$this.attr("data-label")}]`);
+            const maxCount = $this.attr("data-max-words");
+            $this.on("input", () => {
+                const wordsCount = $this.val().split(" ").length;
+                if ($this.val() === "") {
+                    $label.text(maxCount);
+                } else {
+                    $label.text(maxCount - wordsCount);
+                }
+            });
+        });
+    }
+
+    if ($(".registration__photos").length > 0) {
+        let offset = 0;
+        const $window = $(window);
+        const $body = $("body");
+        const $registration = $(".registration");
+        if ($window.width() >= 576) {
+            offset = ($body.width() - $registration.width())/2;
+        }
+        $window.resize(() => {
+            if ($window.width() >= 576) {
+                offset = ($body.width() - $registration.width())/2;
+                $slider.slick("unslick");
+                const updatedOpts = {
+                    mobileFirst: true,
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    centerMode: true,
+                    centerPadding: "60px",
+                    arrows: false,
+                    dots: false,
+                    autoplay: false,
+                    infinite: false,
+                    initialSlide: 1,
+                    responsive: [
+                        {
+                            breakpoint: 576,
+                            settings: {
+                                centerPadding: offset + "px"
+                            }
+                        },
+                        {
+                            breakpoint: 768,
+                            settings: {
+                                slidesToShow: 2,
+                                centerPadding: `calc(${(offset + "px")} - .75rem)`
+                            }
+                        },
+                        {
+                            breakpoint: 992,
+                            settings: {
+                                centerPadding: `calc(${(offset + "px")} - .75rem)`
+                            }
+                        },
+                        {
+                            breakpoint: 1200,
+                            settings: {
+                                centerPadding: `calc(${(offset + "px")} - .75rem)`
+                            }
+                        },
+                        {
+                            breakpoint: 1460,
+                            settings: {
+                                centerPadding: `calc(${(offset + "px")} - .75rem)`
+                            }
+                        }
+                    ]
+                }
+                $slider.slick(updatedOpts);
+            }
+        });
+
+        const initialOpts = {
+            mobileFirst: true,
+            slidesToShow: 2,
+            slidesToScroll: 1,
+            centerMode: true,
+            centerPadding: "60px",
+            arrows: false,
+            dots: false,
+            autoplay: false,
+            infinite: false,
+            initialSlide: 1,
+            responsive: [
+                {
+                    breakpoint: 576,
+                    settings: {
+                        centerPadding: offset + "px"
+                    }
+                },
+                {
+                    breakpoint: 768,
+                    settings: {
+                        slidesToShow: 2,
+                        centerPadding: `calc(${(offset + "px")} - .75rem)`
+                    }
+                },
+                {
+                    breakpoint: 992,
+                    settings: {
+                        centerPadding: `calc(${(offset + "px")} - .75rem)`
+                    }
+                },
+                {
+                    breakpoint: 1200,
+                    settings: {
+                        centerPadding: `calc(${(offset + "px")} - .75rem)`
+                    }
+                },
+                {
+                    breakpoint: 1460,
+                    settings: {
+                        centerPadding: `calc(${(offset + "px")} - .75rem)`
+                    }
+                }
+            ]
+        }
+
+        const $slider = $(".registration__photos");
+        $slider.slick(initialOpts);
+
+        const $delete = $(".registration__photos-item-delete");
+        $delete.click(e => {
+            const $this = $(e.currentTarget);
+            const $item = $this.parent();
+            const index = $item.index();
+            $(".registration__photos-item").eq(index).fadeOut("fast", () => {
+                /* $(".registration__photos-item").eq(index).remove(); */
+                $slider.slick("slickRemove", index);
+            });
+        });
+    }
+
+    if ($(".range").length > 0) {
+        const slider = document.getElementById("range");
+        noUiSlider.create(slider, {
+            start: [21, 33],
+            connect: true,
+            tooltips: true,
+            format: {
+                to: function (value) {
+                    return value + '';
+                },
+                from: function (value) {
+                    return value.replace(',-', '');
+                }
+            },
+            step: 1,
+            range: {
+                "min": 18,
+                "max": 50
+            }
+        });
+    }
     
 });
